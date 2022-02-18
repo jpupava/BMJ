3c7c1c14bb4f7bac2e43209b351045cfae3493f2
Bianka Modrovičová <bibisvk@gmail.com>
2022-02-18 17:31:28 +0100

    add

---
+++ src/main/java/com/example/demo/Book.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+public class Book {
+    private Long id;
+    private String name;
+
+}
---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,78 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class Controller {
+    private List<Customer> customers;
+
+    public Controller() {
+        this.customers = init();
+
+    }
+
+    private List<Customer> init() {
+        List<Customer> customers = new ArrayList<>();
+        Customer customer1 = new Customer();
+        customer1.setId(0L);
+        customer1.setFirstName("Janko");
+        customer1.setLastName("Hraško");
+        customer1.setEmail("jankohrasko@gmail.com");
+        customers.add(customer1);
+
+        Customer customer2 = new Customer();
+        customer2.setId(1L);
+        customer2.setFirstName("Jožko");
+        customer2.setLastName("Mrkvička");
+        customer2.setEmail("jozkomrkvicka@gmail.com");
+        customers.add(customer2);
+
+        return customers;
+    }
+
+    @GetMapping("/api/customers/{customerId}")
+    public Customer getCustomer(@PathVariable Integer customerId){
+        return this.customers.get(customerId);
+    }
+
+    @GetMapping("/api/customers")
+    public List<Customer> getCustomers(@RequestParam(required = false) String customerName){
+
+        List<Customer> filteredCustomers = new ArrayList<>();
+        for (Customer customer : customers){
+            if (customer.getLastName().equals(customerName)){
+                filteredCustomers.add(customer);
+            }
+        }
+        System.out.println(customerName);
+        if(customerName==null){
+            return this.customers;
+        }
+        return filteredCustomers;
+
+    }
+
+    @PostMapping("/api/customers")
+    public Integer createCustomer(@RequestBody Customer customer){
+        this.customers.add(customer);
+        this.customers.get(customers.size()-1).setId(Long.valueOf(customers.size()-1));
+        return this.customers.size()-1;
+    }
+
+    @DeleteMapping("/api/customers/{customerId}")
+    public void deleteCustomer(@PathVariable Integer customerId){
+        this.customers.remove(this.customers.get(customerId));
+    }
+
+    @PutMapping("/api/customers/{customerId}")
+    public void updateCustomer(@PathVariable Integer customerId, @RequestBody Customer customer){
+        this.customers.get(customerId).setId(Long.valueOf(customerId));
+        this.customers.get(customerId).setFirstName(customer.getFirstName());
+        this.customers.get(customerId).setLastName(customer.getLastName());
+        this.customers.get(customerId).setEmail(customer.getEmail());
+    }
+
+}
