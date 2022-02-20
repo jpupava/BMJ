27ab4a936a87e44e0dd9c94c4a14237520ab2a00
mfulajtar <mfulajtar@umb.sk>
2022-02-20 15:08:31 +0100

    Controller s fun. pre BOOK

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -7,11 +7,12 @@ import java.util.List;
 
 @RestController
 public class Controller {
+    /*CUSTOMER*/
     private List<Customer> customers;
 
     public Controller() {
         this.customers = initCustomer();
-
+        this.books = initBook();
     }
 
     private List<Customer> initCustomer() {
@@ -74,5 +75,73 @@ public class Controller {
         this.customers.get(customerId).setLastName(customer.getLastName());
         this.customers.get(customerId).setEmail(customer.getEmail());
     }
+    /*BOOK*/
+    private List<Book> books;
 
+    private List<Book> initBook() {
+        List<Book> books = new ArrayList<>();
+        Book book1 = new Book();
+        book1.setId(0L);
+        book1.setauthorFirstname("Peter");
+        book1.setauthorLastname("Lynch");
+        book1.settitle("One up on Wall Street");
+        book1.setisbn("ISBN12A69C");
+        book1.setcount(10);
+        books.add(book1);
+
+        Book book2 = new Book();
+        book2.setId(0L);
+        book2.setauthorFirstname("Matko");
+        book2.setauthorLastname("Usko");
+        book2.settitle("matko-usko");
+        book2.setisbn("ISBN15A29C");
+        book2.setcount(8);
+        books.add(book2);
+
+        return books;
+    }
+
+    @GetMapping("/api/books/{bookId}")
+    public Book getBook(@PathVariable Integer bookId){
+        return this.books.get(bookId);
+    }
+
+    @GetMapping("/api/books")
+    public List<Book> getBooks(@RequestParam(required = false) String bookTitle){
+
+        List<Book> filteredBooks = new ArrayList<>();
+        for (Book book : books){
+            if (book.gettitle().equals(bookTitle)){
+                filteredBooks.add(book);
+            }
+        }
+        System.out.println(bookTitle);
+        if(bookTitle==null){
+            return this.books;
+        }
+        return filteredBooks;
+
+    }
+
+    @PostMapping("/api/books")
+    public Integer addBook(@RequestBody Book book){
+        this.books.add(book);
+        this.books.get(books.size()-1).setId(Long.valueOf(books.size()-1));
+        return this.books.size()-1;
+    }
+
+    @DeleteMapping("/api/books/{bookId}")
+    public void deleteBook(@PathVariable Integer bookId){
+        this.books.remove(this.books.get(bookId));
+    }
+
+    @PutMapping("/api/customers/{customerId}")
+    public void updateBook(@PathVariable Integer bookId, @RequestBody Book book){
+        this.books.get(bookId).setId(Long.valueOf(bookId));
+        this.books.get(bookId).setauthorFirstname(book.getauthorFirstname());
+        this.books.get(bookId).setauthorLastname(book.getauthorLastname());
+        this.books.get(bookId).settitle(book.gettitle());
+        this.books.get(bookId).setisbn(book.getisbn());
+        this.books.get(bookId).setcount(book.getcount());
+    }
 }
